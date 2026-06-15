fb8acdac4cd4c4c00c6afd9a74490ba7eec8f422
Huewhg <adam.dragon.karasek@gmail.com>
2026-06-15 21:17:44 +0200

    PLEASE LEMME TEST THIS STUFFFFF

--- app.py
+++ app.py
@@ -261,13 +261,22 @@ def discord():
 @app.before_request
 def before():
     print(flask.request.headers)
-    if flask.request.headers.get("X-Forwarded-For", flask.request.remote_addr) == "37.143.117.214":
+    if (
+        flask.request.headers.get("X-Forwarded-For", flask.request.remote_addr)
+        == "37.143.117.214"
+    ):
         flask.abort(404)
-        mail.send_email("ILLEGAL ACCESS DETECTED", f"{flask.request.path}\n{datetime.datetime.now()}\n{flask.request.remote_addr}")
+        mail.send_email(
+            "ILLEGAL ACCESS DETECTED",
+            f"{flask.request.path}\n{datetime.datetime.now()}\n{flask.request.remote_addr}",
+        )
+
 
 @app.route("/test", methods=["GET"])
 def test():
-    return flask.request.headers
+    return flask.jsonify(flask.request.headers)
+
+
 @app.route("/", methods=["GET"])
 def main():
     global last_cpu
